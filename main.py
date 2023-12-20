382cdd270bfccaf5069420ba9d1eb60acc3d563a
denis-verkholantsev <dverholancev@mail.ru>
2023-12-20 03:32:19 +0300

    Fix typo

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from db.models import Base
 from sqlalchemy import create_engine
 
 URL = "postgresql://postgres:1313@localhost:8765/postgres"
-engine = create_engine(URL,echo=True)
+engine = create_engine(URL, echo=True)
 Base.metadata.create_all(bind=engine)
 
 
