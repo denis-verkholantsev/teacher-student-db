c87802fff488899192c3eecd2455d9174ebb11ab
denis-verkholantsev <dverholancev@mail.ru>
2023-12-25 08:30:58 +0300

    Delete all

--- main.py
+++
@@ -1,8 +0,0 @@
-from db.models import Base
-from sqlalchemy import create_engine
-
-URL = "postgresql://postgres:1313@localhost:8765/postgres"
-engine = create_engine(URL, echo=True)
-Base.metadata.create_all(bind=engine)
-
-
